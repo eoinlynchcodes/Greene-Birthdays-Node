395e2bc5363833c2f8042c9f6e4277b8ed3434cc
eoinlynchcodes <eoin95lynch@gmail.com>
2020-05-03 22:28:48 +0100

    initial commit

--- dbConfig.js
+++ dbConfig.js
@@ -1,6 +1,6 @@
 const knex = require('knex');
 
-const config = require('../Greene-Birthdays-Node/knexfile');
+const config = require('../Greene-Birthdays-Node/knexfile.js');
 
 const db = knex(config.development);
 
